ac6c08cc4a7d1a63f1150958c65d7ce669405988
Ya-shu1 <103036816+Ya-shu1@users.noreply.github.com>
2023-12-28 12:50:38 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,8 @@
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo chmod 666 /var/run/docker.sock
+git clone https://github.com/GOUSERABBANI44/Node_APP.git
+cd Node_APP
+sudo docker build -t yashu12/nodejs .
+sudo docker run -t --name newcontainer -p 3000:3000 yashu12/nodejs
