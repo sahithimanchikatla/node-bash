5ebd52d65d1b7c573053bad409844130e417b7b9
sahithimanchikatla <145747310+sahithimanchikatla@users.noreply.github.com>
2023-12-29 11:47:10 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,5 +4,5 @@ sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 git clone https://github.com/GOUSERABBANI44/Node_APP.git
 cd Node_APP
-sudo docker build -t yashu12/nodejs .
-sudo docker run -t --name newcontainer -p 3000:3000 yashu12/nodejs
+sudo docker build -t hasini12/nodejs .
+sudo docker run -t --name newcontainer -p 3000:3000 hasini12/nodejs
